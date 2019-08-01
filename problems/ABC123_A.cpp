661d195394a509c633e3ca9a61b3f37d6acf286c
sokazaki <okazakisoichiro@gmail.com>
2019-08-02 07:03:32 +0900

    Create ABC123_A.cpp

---
+++ problems/ABC123_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a, b, c, d, e, k;
+    cin >> a >> b >> c >> d >> e >> k;
+ 
+    if (e-a <= k) {
+      cout << "Yay!" << endl;
+    } else {
+      cout << ":(" << endl;
+    }
+ 
+    return 0;
+}
