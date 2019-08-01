d0e3270f4a95e7efa005cb360f6381912d8f3bff
sokazaki <okazakisoichiro@gmail.com>
2019-08-01 22:53:38 +0900

    Update ABC132_A.cpp

--- problems/ABC132_A.cpp
+++ problems/ABC132_A.cpp
@@ -4,15 +4,15 @@
 using namespace std;
  
 int main() {
-  string S;
-  cin >> S;
-  
-  sort(S.begin(), S.end());
-  
-  if (S[0]==S[1] && S[1]!=S[2] && S[2]==S[3]) {
-    cout << "Yes" << endl;
-  } else {
-    cout << "No" << endl;
-  }
-  return 0;
+    string S;
+    cin >> S;
+
+    sort(S.begin(), S.end());
+
+    if (S[0]==S[1] && S[1]!=S[2] && S[2]==S[3]) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    }
+    return 0;
 }
