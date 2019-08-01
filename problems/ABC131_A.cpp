c9cc01f17fb438120149bf5e2ca7996356ec664a
sokazaki <okazakisoichiro@gmail.com>
2019-08-01 22:53:16 +0900

    Update ABC131_A.cpp

--- problems/ABC131_A.cpp
+++ problems/ABC131_A.cpp
@@ -3,13 +3,13 @@
 using namespace std;
  
 int main() {
-  string S;
-  cin >> S;
+    string S;
+    cin >> S;
   
-  if (S[0]==S[1] or S[1]==S[2] or S[2]==S[3]) {
-    cout << "Bad" << endl;
-  } else {
-    cout << "Good" << endl;
-  }
-  return 0;
+    if (S[0]==S[1] or S[1]==S[2] or S[2]==S[3]) {
+      cout << "Bad" << endl;
+    } else {
+      cout << "Good" << endl;
+    }
+    return 0;
 }
