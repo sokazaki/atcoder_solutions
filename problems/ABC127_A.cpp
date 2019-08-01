ab645522d70d0f95dad63da38f2cdef35d671e25
sokazaki <okazakisoichiro@gmail.com>
2019-08-02 06:30:03 +0900

    Update ABC127_A.cpp

--- problems/ABC127_A.cpp
+++ problems/ABC127_A.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int A, B;
     cin >> A >> B;
- 
-  	if (A>=13) {
+
+    if (A>=13) {
       cout << B << endl;
     } else if (A>=6) {
       cout << B/2 << endl;
