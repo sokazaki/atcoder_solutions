86cfd684ddb9d25c621b36a89faebc2c8d6e8282
sokazaki <okazakisoichiro@gmail.com>
2019-08-01 22:10:45 +0900

    Create ABC131_A.cpp

---
+++ problems/ABC131_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(){
+  string S;
+  cin >> S;
+  
+  if (S[0]==S[1] or S[1]==S[2] or S[2]==S[3]) {
+    cout << "Bad" << endl;
+  } else {
+    cout << "Good" << endl;
+  }
+  return 0;
+}
