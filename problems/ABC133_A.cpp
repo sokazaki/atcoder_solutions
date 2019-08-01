09bbf3b36e4baf060ab19a8796e8dc42f3b8e7c7
sokazaki <okazakisoichiro@gmail.com>
2019-08-01 22:54:06 +0900

    Update ABC133_A.cpp

--- problems/ABC133_A.cpp
+++ problems/ABC133_A.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
     int N, A, B;
     cin >> N >> A >> B;
- 
+
     if (B>=N*A) {
       cout << N*A << endl;
     } else {
       cout << B << endl;
     }
- 
+
     return 0;
 }
